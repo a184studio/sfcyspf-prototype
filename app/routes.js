17093d42aa2d346ba45f0eefccd6a1c54c50bf47
Mark McCormick <mark@a184.uk>
2019-11-18 11:43:10 +0000

    MM: SFC / A.0.3

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 router.use('/sprint-1', require('./routes/sprint-1'))
 router.use('/sprint-2', require('./routes/sprint-2'))
+router.use('/sprint-3', require('./routes/sprint-3'))
 
 
 module.exports = router
