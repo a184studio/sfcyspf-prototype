5675f9ca091cc11def3ab3aeddcfcc37bc6917d9
Mark McCormick <mark@a184.uk>
2019-11-27 11:48:24 +0000

    MM: SFC A.05

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,7 @@ router.use('/sprint-1', require('./routes/sprint-1'))
 router.use('/sprint-2', require('./routes/sprint-2'))
 router.use('/sprint-3', require('./routes/sprint-3'))
 router.use('/sprint-4', require('./routes/sprint-4'))
+router.use('/sprint-5', require('./routes/sprint-5'))
 
 
 module.exports = router
