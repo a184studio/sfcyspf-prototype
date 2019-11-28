9b25e7ae2dfc0c50f3aae63a764694853cba30ae
Mark McCormick <mark@a184.uk>
2019-11-28 18:16:23 +0000

    MM: SFC / A.0.5

--- app/routes/sprint-5/index.js
+++ app/routes/sprint-5/index.js
@@ -1,260 +1,143 @@
 const express = require('express')
 const router = new express.Router()
 
-
-
-
-
   router.post('/start-work-router', (req, res, next) => {
     const workCheck = req.session.data['start-work']
 
     if (workCheck === 'Telephone') {
-      res.redirect('yourself-national-insurance')
+      res.redirect('telephone-national-insurance-check')
     } else if (workCheck === 'Paper') {
-      res.redirect('paper-national-insurance')
+      res.redirect('paper-national-insurance-check')
     } else {
       res.redirect('xxx')
     }
   })
 
-  router.post('/who-is-caller-router', (req, res, next) => {
-    const whoIsCaller = req.session.data['who-is-caller']
+  router.post('/telephone-national-insurance-check-router', (req, res, next) => {
+    const telephoneNIcheck = String(req.session.data['telephone-national-insurance-check']).toUpperCase()
 
-  if (whoIsCaller === 'Citizen') {
-    res.redirect('yourself-national-insurance')
-  } else if (whoIsCaller === 'Corporate Acting Body (CAB)') {
-    res.redirect('adviser-id-check')
-  } else if (whoIsCaller === 'Personal Acting Body (PAB)') {
-    res.redirect('adviser-id-check')
-  } else if (whoIsCaller === 'Third party (EG)') {
-    res.redirect('adviser-id-unofficial')
+    if (telephoneNIcheck === 'E1') {
+      res.redirect('exclusion-1')
+    } else if (telephoneNIcheck === 'E2') {
+      res.redirect('exclusion-2')
+    } else if (telephoneNIcheck === 'E3') {
+      res.redirect('exclusion-3')
+    } else if (telephoneNIcheck === 'E4') {
+      res.redirect('exclusion-4')
+    } else if (telephoneNIcheck === 'E5') {
+      res.redirect('exclusion-5')
+    } else if (telephoneNIcheck === 'E6') {
+      res.redirect('exclusion-6')
+    } else if (telephoneNIcheck === 'E7') {
+      res.redirect('exclusion-7')
+    } else if (telephoneNIcheck === 'E8') {
+      res.redirect('exclusion-8')
+    } else if (telephoneNIcheck === 'E9') {
+      res.redirect('exclusion-9')
+    } else if (telephoneNIcheck === 'E10') {
+      res.redirect('exclusion-10')
+    } else if (telephoneNIcheck === 'RRE') {
+      res.redirect('telephone-rre')
+    } else if (telephoneNIcheck === 'NOCOPE') {
+      res.redirect('telephone-cope-check')
+    } else {
+      res.redirect('validation')
+    }
+  })
+
+  router.post('/paper-national-insurance-check-router', (req, res, next) => {
+    const paperNIcheck = String(req.session.data['paper-national-insurance-check']).toUpperCase()
+
+    if (paperNIcheck === 'E1') {
+      res.redirect('exclusion-1')
+    } else if (paperNIcheck === 'E2') {
+      res.redirect('exclusion-2')
+    } else if (paperNIcheck === 'E3') {
+      res.redirect('exclusion-3')
+    } else if (paperNIcheck === 'E4') {
+      res.redirect('exclusion-4')
+    } else if (paperNIcheck === 'E5') {
+      res.redirect('exclusion-5')
+    } else if (paperNIcheck === 'E6') {
+      res.redirect('exclusion-6')
+    } else if (paperNIcheck === 'E7') {
+      res.redirect('exclusion-7')
+    } else if (paperNIcheck === 'E8') {
+      res.redirect('exclusion-8')
+    } else if (paperNIcheck === 'E9') {
+      res.redirect('exclusion-9')
+    } else if (paperNIcheck === 'E10') {
+      res.redirect('exclusion-10')
+    } else if (paperNIcheck === 'RRE') {
+      res.redirect('paper-rre')
+    } else if (paperNIcheck === 'NOCOPE') {
+      res.redirect('paper-cope-check')
+    } else {
+      res.redirect('validation')
+    }
+  })
+
+  router.post('/telephone-cope-check-router', (req, res, next) => {
+    const telephoneCOPEcheck = req.session.data['telephone-cope-check']
+
+    if (telephoneCOPEcheck === 'No') {
+      res.redirect('telephone-forecast-enquiry')
+    } else if (telephoneCOPEcheck === 'Yes') {
+      res.redirect('outcome-no-cope')
+    } else {
+      res.redirect('xxx')
+    }
+  })
+
+  router.post('/paper-cope-check-router', (req, res, next) => {
+    const paperCOPEcheck = req.session.data['paper-cope-check']
+
+    if (paperCOPEcheck === 'Yes') {
+      res.redirect('outcome-no-cope')
+    } else {
+      res.redirect('who-is-caller')
+    }
+  })
+
+  router.post('/telephone-forecast-enquiry-router', (req, res, next) => {
+    const telephoneForecastEnquiry = req.session.data['telephone-forecast-enquiry']
+
+    if (telephoneForecastEnquiry === 'Forecast') {
+      res.redirect('online-prompt')
+    } else if (telephoneForecastEnquiry === 'Enquiry') {
+      res.redirect('telephone-statement-check')
+    } else {
+      res.redirect('xxx')
+    }
+  })
+
+
+  router.post('/address-check-router', (req, res, next) => {
+    const addressCheck = req.session.data['address-check']
+
+    if (addressCheck === 'United Kingdom') {
+      res.redirect('address-postcode')
+    } else if (addressCheck === 'Overseas') {
+      res.redirect('address-manual')
+    } else {
+      res.redirect('XXX')
+    }
+  })
+
+
+
+router.post('/telephone-statement-check-router', (req, res, next) => {
+  const telephoneStatementCheck = req.session.data['telephone-statement-check']
+
+  if (telephoneStatementCheck === 'Yes') {
+    res.redirect('telephone-overview')
   } else {
-    res.redirect('paper-who-is-caller')
-  }
-})
-
-router.post('/paper-who-is-caller-router', (req, res, next) => {
-  const whoIsCaller = req.session.data['paper-who-is-caller']
-
-if (whoIsCaller === 'Citizen') {
-  res.redirect('paper-address-check')
-} else if (whoIsCaller === 'Corporate Acting Body (CAB)') {
-  res.redirect('paper-address-check')
-} else if (whoIsCaller === 'Personal Acting Body (PAB)') {
-  res.redirect('paper-address-check')
-} else if (whoIsCaller === 'Third party (EG)') {
-  res.redirect('paper-address-check')
-} else {
-  res.redirect('paper-who-is-caller')
-}
-})
-
-
-router.post('/who-is-caller-other-router', (req, res, next) => {
-  const whoIsCaller = req.session.data['who-is-caller-other']
-
-  if (whoIsCaller === 'Personal capacity') {
-    res.redirect('legal-adviser-id-check')
-  } else if (whoIsCaller === 'Organisation') {
-    res.redirect('notlegal-adviser-id-check')
-  } else {
-    res.redirect('XXX')
-  }
-})
-
-
-
-router.post('/yourself-cope-check-yn-router', (req, res, next) => {
-  const whoIsCaller = req.session.data['yourself-cope-check-yn']
-
-  if (whoIsCaller === 'No') {
-    res.redirect('telephone-who-is-caller')
-  } else if (whoIsCaller === 'Yes') {
-    res.redirect('outcome-no-cope')
-  } else {
-    res.redirect('XXX')
-  }
-})
-
-router.post('/paper-cope-check-router', (req, res, next) => {
-  const whoIsCaller = req.session.data['paper-cope-check-yn']
-
-  if (whoIsCaller === 'No') {
-    res.redirect('paper-who-is-caller')
-  } else if (whoIsCaller === 'Yes') {
-    res.redirect('paper-outcome-no-cope')
-  } else {
-    res.redirect('XXX')
-  }
-})
-
-
-
-router.post('/paper-address-check-router', (req, res, next) => {
-  const addressCheck = req.session.data['paper-address-check']
-
-  if (addressCheck === 'United Kingdom') {
-    res.redirect('paper-address-postcode')
-  } else if (addressCheck === 'Overseas') {
-    res.redirect('paper-address-manual')
-  } else {
-    res.redirect('XXX')
-  }
-})
-
-
-
-router.post('/yourself-find-claimant-router', (req, res, next) => {
-  const addressCheck = req.session.data['yourself-find-claimant-workAddress']
-
-  if (addressCheck === 'United Kingdom') {
-    res.redirect('yourself-address-postcode')
-  } else if (addressCheck === 'Overseas') {
-    res.redirect('yourself-address-manual')
-  } else {
-    res.redirect('XXX')
-  }
-})
-
-router.post('/yourself-ni-found-yn-router', (req, res, next) => {
-  const niFound = req.session.data['yourself-ni-found-yn']
-
-  if (niFound === 'Yes') {
-    res.redirect('yourself-overview-options')
-  } else if (niFound === 'No') {
-    res.redirect('yourself-adviser-prompt')
-  } else {
-    res.redirect('yourself-adviser-prompt')
-  }
-})
-
-router.post('/yourself-live-in-uk-router', (req, res, next) => {
-  const yourselfLiveInUk = req.session.data['yourself-live-in-uk']
-
-  if (yourselfLiveInUk === 'Yes') {
-    res.redirect('yourself-address-postcode')
-  } else if (yourselfLiveInUk === 'No') {
-    res.redirect('yourself-address-manual')
-  } else {
-    res.redirect('yourself-adviser-fallout')
-  }
-})
-
-
-router.post('/yourself-employment-yn-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-employment-yn']
-
-  if (yourselfEmploymentYn === 'Yes') {
-    res.redirect('yourself-employment-type')
-  } else if (yourselfEmploymentYn === 'No') {
-    res.redirect('yourself-employment-none')
-  } else {
-    res.redirect('yourself-adviser-fallout')
-  }
-})
-
-router.post('/yourself-employment-type-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-employment-type']
-
-  if (yourselfEmploymentYn === 'Employed in the UK') {
-    res.redirect('yourself-employment-yes')
-  } else if (yourselfEmploymentYn === 'Self-employed in the UK') {
-    res.redirect('yourself-self-employment-yes')
-  } else {
-    res.redirect('yourself-overview-not-gaining')
-  }
-})
-
-router.post('/yourself-employment-none-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-employment-none']
-
-  if (yourselfEmploymentYn === 'Yes - I am receiving credits') {
-    res.redirect('outcome-forecast-a')
-  } else if (yourselfEmploymentYn === 'Yes - I am on benefits') {
-    res.redirect('outcome-forecast-a')
-  } else {
-    res.redirect('yourself-overview-not-gaining')
+    res.redirect('telephone-online-prompt-forecast')
   }
 })
 
 
 
-
-
-
-router.post('/yourself-employment-yes-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-employment-yes']
-
-  if (yourselfEmploymentYn === 'Yes') {
-    res.redirect('yourself-employment-end-known')
-  } else if (yourselfEmploymentYn === 'No') {
-    res.redirect('yourself-overview-not-gaining')
-  } else {
-    res.redirect('yourself-overview-not-gaining')
-  }
-})
-
-router.post('/yourself-self-employment-yes-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-self-employment-yes']
-
-  if (yourselfEmploymentYn === 'Yes') {
-    res.redirect('yourself-self-employment-end-known')
-  } else if (yourselfEmploymentYn === 'No') {
-    res.redirect('yourself-overview-not-gaining')
-  } else {
-    res.redirect('yourself-overview-not-gaining')
-  }
-})
-
-
-router.post('/yourself-employment-end-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-employment-end']
-
-  if (yourselfEmploymentYn === 'Yes') {
-    res.redirect('yourself-employment-end-known')
-  } else if (yourselfEmploymentYn === 'No') {
-    res.redirect('outcome-forecast-a')
-  } else {
-    res.redirect('outcome-forecast-a')
-  }
-})
-
-router.post('/yourself-employment-end-known-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-employment-end-known']
-
-  if (yourselfEmploymentYn === 'I reach a particular age') {
-    res.redirect('outcome-bespoke')
-  } else if (yourselfEmploymentYn === 'I reach a particular date') {
-    res.redirect('outcome-bespoke')
-  } else {
-    res.redirect('outcome-forecast-a')
-  }
-})
-
-router.post('/yourself-self-employment-class2-end-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-self-employment-class2-end']
-
-  if (yourselfEmploymentYn === 'Yes') {
-    res.redirect('yourself-self-employment-class2-end-known')
-  } else if (yourselfEmploymentYn === 'No') {
-    res.redirect('outcome-forecast-a')
-  } else {
-    res.redirect('outcome-forecast-a')
-  }
-})
-
-
-router.post('/yourself-self-employment-class2-end-known-router', (req, res, next) => {
-  const yourselfEmploymentYn = req.session.data['yourself-self-employment-class2-end']
-
-  if (yourselfEmploymentYn === 'I reach a particular age') {
-    res.redirect('outcome-bespoke')
-  } else if (yourselfEmploymentYn === 'I reach a particular date') {
-    res.redirect('outcome-bespoke')
-  } else {
-    res.redirect('outcome-forecast-a')
-  }
-})
 
 
 
